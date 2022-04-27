70fa24b20e69da5cb3ab8f0917b5f732a68f63b5
mariana-oliveiraaa <99214209+mariana-oliveiraaa@users.noreply.github.com>
2022-04-27 09:35:05 -0300

    Add files via upload

---
+++ vue-base-exercicio-1-master/src/components/MainPrincipal.vue
@@ -0,0 +1,12 @@
+<template>
+  <main id="content">
+    <article class="block">informatica</article>
+    <article class="block">Informatica</article>
+    <article class="block">Informatica</article>
+    <article class="block">Informatica</article>
+    <article class="block">Informatica</article>
+    <article class="block">Informatica</article>
+
+   
+  </main>
+</template>
---
+++ vue-base-exercicio-1-master/src/components/MenuLateral.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+<template>
+  <aside id="sidenav">
+    <ul>
+      <li>Sobre o Curso</li>
+      <li>Materias</li>
+      <li>Professores</li>
+      <li>Horarios</li>
+      <li>Tempo de Curso</li>
+    </ul>
+  </aside>
+</template>
+<style></style>
